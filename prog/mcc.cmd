4a24aff590f86b9759f7d2eda03418c09bcaae10
Zoltan Pekic <zpekic@hotmail.com>
2023-10-01 12:37:10 -0700

    Adding helloworld test program (incomplete)
    Refactored test programs with some includes, which made some compiler fixes necessary.
    
    Added aliases and fixed bug where A register was ".regvalue" and all other are ".value"

--- prog/mcc.cmd
+++ prog/mcc.cmd
@@ -1,5 +1,6 @@
 @echo off
 @echo -- Compile code for internal and external ROMs
 @echo See https://github.com/zpekic/MicroCodeCompiler
-..\..\Sys9900\mcc\mcc\bin\Debug\mcc.exe /a:sif fillscreen.sif
+@rem ..\..\Sys9900\mcc\mcc\bin\Debug\mcc.exe /a:sif fillscreen.sif
+..\..\Sys9900\mcc\mcc\bin\Debug\mcc.exe /a:sif helloworld.sif
 
